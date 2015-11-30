23920676347b5a824f8c146df72d9342903fbe36
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2015-11-30 07:11:28 +0200

    Word ladder done

--- selflearning/cs106b programming abstractions/Assignment2/WordLadder/src/WordLadder.cpp
+++ selflearning/cs106b programming abstractions/Assignment2/WordLadder/src/WordLadder.cpp
@@ -17,32 +17,61 @@
 using namespace std;
 
 
-Queue<string> wordLadder(string source, string dest);
-
+Vector<string> wordLadder(string source, string dest);
+void print(const Vector<string> & vec);
 int main() {
-	// [TODO: fill in the code]
+    cout << "This program creates ladder between two words!" << endl;
+    while (true) {
+        string source = getLine("The source word (Enter to exit): ");
+        if (source == "") break;
+        string dest = getLine("The destination word: ");
+        Vector<string> ladder = wordLadder(source, dest);
+        if (ladder.size() == 1)
+            cout << "There is no ladder between those two words!!" << endl;
+        else
+            print(ladder);
+    }
 	return 0;
 }
 
+void print(const Vector<string> & vec) {
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << endl;
+}
 
-Queue<string> wordLadder(string source, string dest) {
+Vector<string> wordLadder(string source, string dest) {
+    Vector<string> ladder;
+    ladder += source;
     Lexicon english("EnglishWords.dat");
+    if (source.length() != dest.length() || !english.contains(source) || !english.contains(dest))
+        return ladder;
     Queue< Vector<string> > ladders;
-    Vector<string> ladder += source;
-    set<string> usedWords;
-    ladders.enqueue(source);
+    Lexicon usedWords;
+    usedWords.add(source);
     ladders.enqueue(ladder);
+
     while (!ladders.isEmpty()) {
-        Vector<string> words = ladder.dequeue();
-        if (word[words.size() - 1] == dest) {
-            return ladder;
+        Vector<string> words = ladders.dequeue();
+        string currWord = words[words.size() - 1];
+        if (currWord == dest) {
+            return words;
         } else {
-            string str = words[words.size() - 1];
-            for (char ch = str[0]; ch <= 'z'; ch++) {
-                for ()
+            for (int i = 0; i  <  currWord.length(); i++) {
+                string str = currWord;
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    str[i] = ch;
+                    if(english.contains(str) && !usedWords.contains(str)) {
+                        usedWords.add(str);
+                        Vector<string> newLadder = words;
+                        newLadder += str;
+                        ladders.enqueue(newLadder);
+                    }
+                }
             }
 
         }
 
     }
+    return ladder;
 }
